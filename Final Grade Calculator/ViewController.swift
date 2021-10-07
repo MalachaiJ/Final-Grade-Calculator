144cb9947d66c5eb6b469e189745e91cc05f0406
Makachai Jacobs <malachai.jacobs15@gmail.com>
2021-10-07 10:55:23 -0600

    Initial Commit

---
+++ Final Grade Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Final Grade Calculator
+//
+//  Created by Malachai Jacobs on 10/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
